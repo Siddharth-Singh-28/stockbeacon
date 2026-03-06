3062015b896e55de1bc391520780727ab2619926
Siddharth-Singh-28 <siddharthsingh133@gmail.com>
2026-03-06 21:24:33 +0530

    feat: add homepage layout with header, footer and trending stocks UI

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
